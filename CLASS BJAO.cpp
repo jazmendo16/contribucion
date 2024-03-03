ae6ffbbc01874743329a0fc131a8658e9dc97dea
BAlvarez12 <160418757+BAlvarez12@users.noreply.github.com>
2024-03-03 11:27:53 -0600

    ACTIVIDAD 2
    ACTIVIDAD 2

---
+++ CLASS BJAO.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+class BJAO{
+
+    private : string nombre, apellido, direccion;
+    
+    void crear();
+    void leer ();
+    void  actualizar();
+    void borrar();
+    
+};
