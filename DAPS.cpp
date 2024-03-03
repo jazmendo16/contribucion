b05f510bd343105c2e55d4740facd4941689dcf3
DaryPerez <161182193+DaryPerez@users.noreply.github.com>
2024-03-03 11:38:26 -0600

    Add files via upload

---
+++ DAPS.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+class DAPS {
+	private: string nombres, apellidos, direccion;
+	
+	void crear ();
+	void leer ();
+	void actualizar ();
+	void borrar ();
+};
+
